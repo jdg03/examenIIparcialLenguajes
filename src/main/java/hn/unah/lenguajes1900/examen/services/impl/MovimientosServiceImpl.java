dd09f5d3a44012f2c126cc289cc4502ffd480f62
jdg03 <gonzalez5.jose52@gmail.com>
2024-04-05 19:29:13 -0600

    5

---
+++ src/main/java/hn/unah/lenguajes1900/examen/services/impl/MovimientosServiceImpl.java
@@ -0,0 +1,23 @@
+package hn.unah.lenguajes1900.examen.services.impl;
+
+import org.springframework.stereotype.Service;
+
+import hn.unah.lenguajes1900.examen.entities.Movimientos;
+import hn.unah.lenguajes1900.examen.services.MovimientoService;
+
+@Service
+public class MovimientosServiceImpl implements MovimientoService{
+
+    @Override
+    public Movimientos CrearMovimiento(Movimientos movimiento) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'CrearMovimiento'");
+    }
+
+    @Override
+    public Movimientos ObtenerMovimientosPorCuenta(String numeroCuenta) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'ObtenerMovimientosPorCuenta'");
+    }
+    
+}
